0719c907b33b43d818f3c6562128568e37922343
Jere Sotaniemi <jere.sotaniemi@gmail.com>
2025-12-06 02:16:53 +0200

    add PORT to e2e tests on workflow for possible fix

--- playwright.config.js
+++ playwright.config.js
@@ -73,11 +73,6 @@ export default defineConfig({
      command: 'npm run start',
      url: 'http://localhost:5173',
      reuseExistingServer: !process.env.CI,
-     env: {
-      TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || '',
-      SECRET: process.env.SECRET || '',
-      NODE_ENV: 'test',
-  },
   },
 });
 
